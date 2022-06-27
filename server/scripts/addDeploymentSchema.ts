5a1232ba7cf4be6119cf3879fe3cf0936e0539a8
twest6 <twest6@ncsu.edu>
2022-06-27 16:54:28 +0000

    Adding SCADS schemas

--- server/scripts/addDeploymentSchema.ts
+++ server/scripts/addDeploymentSchema.ts
@@ -1,13 +1,13 @@
 import { createSchema } from '../services/schema'
 import { connectToMongoose, disconnectFromMongoose } from '../utils/database'
 
-import minimal from './example_schemas/minimal_deployment_schema.json'
+import minimal from './example_schemas/las_scads_deployment_schema.json'
 ;(async () => {
   await connectToMongoose()
 
   await createSchema({
-    name: 'Minimal Deployment Schema v6',
-    reference: '/Minimal/Deployment/v6',
+    name: 'LAS SCADS Deployment Schema v1',
+    reference: '/LAS/SCADS/Deployment/v1',
     schema: minimal,
     use: 'DEPLOYMENT',
   })
